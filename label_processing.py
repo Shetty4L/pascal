bc8e79c409eb761321ec58b799a4649f07e16097
Ubuntu <ubuntu@ip-172-31-5-173.ec2.internal>
2016-07-10 14:08:52 +0000

    Ran model2 after data augmentation

--- label_processing.py
+++ label_processing.py
@@ -7,11 +7,12 @@ i = 0
 data = pd.DataFrame()
 train = pd.DataFrame()
 val = pd.DataFrame()
-for line in file('filenames.txt'):
-    temp = pd.read_table('formatted/%s'%line[:-1],delimiter=',',names=['fname','true'],
+directory = 'data/VOCdevkit/VOC2011/ImageSets'
+for line in file('%s/filenames.txt'%directory):
+    temp = pd.read_table('%s/formatted/%s'%(directory,line[:-1]),delimiter=',',names=['fname','true'],
                          header=None,dtype={'fname':object,'true':np.int32})
     temp = temp[temp['true']==1]
-    temp['fname'] = temp['fname'] + '.png'
+    temp['fname'] = temp['fname'] + '.jpg'
     temp['true'] = i
     temp2 = pd.DataFrame()
     temp2['fname'] = 'crop1_' + temp['fname']
@@ -22,32 +23,28 @@ for line in file('filenames.txt'):
     temp4 = pd.DataFrame()
     temp4['fname'] = 'crop3_' + temp['fname']
     temp4['true'] = i
-    temp5 = pd.DataFrame()
-    temp5['fname'] = 'crop4_' + temp['fname']
-    temp5['true'] = i
     temp6 = pd.DataFrame()
     temp6['fname'] = 'flip_' + temp['fname']
     temp6['true'] = i
     temp = temp.append(temp2)
     temp = temp.append(temp3)
     temp = temp.append(temp4)
-    temp = temp.append(temp5)
     temp = temp.append(temp6)
     data = data.append(temp)
     i = i + 1
 
 data = data.drop_duplicates('fname',keep='first')
 data = data.sample(n=data.shape[0],replace=True)
-num_train = int(data.shape[0] * 0.7)
+num_train = int(data.shape[0] * 0.6)
 
 train = data[:num_train]
 val = data[num_train:]
 
-train.to_csv('../train.txt',sep=' ',index=None,header=None,mode='w')
-val.to_csv('../val.txt',sep=' ',index=None,header=None,mode='w')
+train.to_csv('train.txt',sep=' ',index=None,header=None,mode='w')
+val.to_csv('val.txt',sep=' ',index=None,header=None,mode='w')
 
 filenames = []
-for line in file('filenames.txt'):
+for line in file('%s/filenames.txt'%directory):
     filenames.append(line[:-1])
 filenames = np.array(filenames)
 
